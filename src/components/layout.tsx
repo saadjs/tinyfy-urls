7924c4f99baaa15f076d6b4c3308bc201bfa40fd
Saad Bash <saadbash08@gmail.com>
2024-04-14 23:40:58 -0400

    layout.tsx: added footer

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,13 +4,20 @@ import { LayoutProps } from "../types/index.ts";
 
 export const Layout = ({ title, children }: PropsWithChildren<LayoutProps>) =>
   html`<!DOCTYPE html>
-  <html>
+  <html style="height: 100%">
     <head>
-    <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
+      <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css">
       <title>${title}</title>
     </head>
-    <body>
-    <h3>Tinyfy: Making Links Littler and Lives Easier!</h3>
-      ${children}
+    <body style="height: 100%">
+      <div>
+        <h3>Tinyfy: Making Links Littler and Lives Easier!</h3>
+        ${children}
+      </div>
+      <footer style="position: sticky; top: 100vh; display: flex; justify-content: center; align-items: center">
+        <div>Saad Bash</div>
+        <div>&ensp;•&ensp;</div>
+        <div>${new Date().getFullYear()}</div>
+      </footer>
     </body>
   </html>`;
